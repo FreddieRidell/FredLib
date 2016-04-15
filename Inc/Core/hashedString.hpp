75cb5f434ba31e387fbb6a00592e31c125cc11e8
Freddie Ridell <DrVonTrap@gmail.com>
2016-04-15 16:26:05 +0100

    HashedString comparason tests

--- Inc/Core/hashedString.hpp
+++ Inc/Core/hashedString.hpp
@@ -14,7 +14,7 @@ int constexpr strLength(const char* str)
 
 class HashedString{
 public:
-    const std::shared_ptr<const char> internalStdString;
+    const char* internalStdString;
     const size_t stringLength;
     const unsigned long hash;
 
@@ -23,21 +23,39 @@ public:
 	stringLength(strLength(0)),
 	hash(0){};
 
-    HashedString (const std::string& str):
-	internalStdString(str.c_str()),
-	stringLength(str.length()),
-	hash(hashStdStr(str)){};
-
-    constexpr HashedString (const char* s):
+    constexpr HashedString(const char* s):
 	internalStdString(s),
 	stringLength(strLength(s)),
 	hash(hashConstStr(s)){};
 
-    HashedString (std::string&& str) noexcept:
+    HashedString(const std::string& str):
 	internalStdString(str.c_str()),
 	stringLength(str.length()),
-	hash(hashStdString(str)){};
+	hash(hashStdStr(str)){};
 
+    HashedString(std::string&& str) noexcept:
+	internalStdString(str.c_str()),
+	stringLength(str.length()),
+	hash(hashStdStr(str)){};
+
+    bool operator==(const HashedString& rhs)const{
+	return hash == rhs.hash;
+    }
+    bool operator!=(const HashedString& rhs)const{
+	return !(*this == rhs);
+    }
+    bool operator>(const HashedString& rhs)const{
+	return hash > rhs.hash;
+    }
+    bool operator<(const HashedString& rhs)const{
+	return hash < rhs.hash;
+    }
+    bool operator>=(const HashedString& rhs)const{
+	return !(*this < rhs);
+    }
+    bool operator<=(const HashedString& rhs)const{
+	return !(*this > rhs);
+    }
 };
 
 } //core
--- Test/Core/Hash/hashedString.cpp
+++ Test/Core/Hash/hashedString.cpp
@@ -10,5 +10,26 @@ TEST(HashedString, CanInitalise){
     static_assert(hs.hash == 193491849, "Hash not calculated at compile");
 }
 
+TEST(HashedString, OpperatorsWork){
+    HashedString foo1("foo");
+    HashedString foo2("foo");
+    HashedString bar("bar");
+
+    EXPECT_TRUE (foo1 == foo2);
+    EXPECT_FALSE(foo1 != foo2);
+    EXPECT_FALSE(foo1 >  foo2);
+    EXPECT_FALSE(foo1 <  foo2);
+    EXPECT_TRUE (foo1 >= foo2);
+    EXPECT_TRUE (foo1 <= foo2);
+
+    EXPECT_FALSE(foo1 == bar);
+    EXPECT_TRUE (foo1 != bar);
+    EXPECT_TRUE (foo1 >  bar);
+    EXPECT_FALSE(foo1 <  bar);
+    EXPECT_TRUE (foo1 >= bar);
+    EXPECT_FALSE(foo1 <= bar);
+}
+
+
 TEST(HASHEDSTRING, FailTest){EXPECT_TRUE(false);}
 
